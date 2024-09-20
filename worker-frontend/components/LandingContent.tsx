c70bc3f775305709d8a757cede125c38da057e79
Sunny <swarnendughosh05@gmail.com>
2024-09-20 11:55:46 +0530

    text changed

--- worker-frontend/components/LandingContent.tsx
+++ worker-frontend/components/LandingContent.tsx
@@ -3,9 +3,13 @@ import React from 'react'
 export const LandingContent:React.FC  = () => {
   return (
     <div> 
-        <div className='flex justify-center py-10 bg-gradient-to-br text-transparent bg-clip-text from-purple-700 to-emerald-700 text-3xl font-bold'>
-           Chosse the best Thumbnail with Turkify
+        <div className='flex justify-center py-10 '>
+            <div className='bg-gradient-to-br text-transparent bg-clip-text from-purple-700 to-emerald-700 lg:text-4xl text-2xl font-bold'>
+                 Vote the thumbnails and get paid in Solana
+            </div> 
+             
         </div>
+         
     </div>
   )
 }
