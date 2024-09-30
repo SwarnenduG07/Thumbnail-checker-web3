1f25d55ffe721f47e8de4755b953832d45b98a99
Sunny <swarnendughosh05@gmail.com>
2024-09-30 08:21:55 +0530

    user forentend edeitd

--- user-frontend/components/Hero.tsx
+++ user-frontend/components/Hero.tsx
@@ -1,10 +1,10 @@
 
 export const Hero = () => {
     return <div className="text-black pt-10">
-        <div className="text-2xl flex justify-center">
+        <div className="flex justify-center font-semibold text-3xl">
             Welcome to Turkify
         </div>
-        <div className="text-lg flex justify-center pt-8">
+        <div className="font-sans text-2xl flex justify-center pt-8">
             Your one stop destination to getting your data labelled
         </div>
     </div>
